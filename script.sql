a8f4f1d36d2bc8c19c3a01b79bdb1a2e95609a8d
Jo3y_the_Creator <ejusticecudjoe@gmail.com>
2021-11-29 16:16:07 +0000

    Final Commit

---
+++ script.sql
@@ -0,0 +1,76 @@
+SELECT *
+FROM books
+LIMIT 10;
+
+SELECT *
+FROM customers
+LIMIT 10;
+
+SELECT *
+FROM orders
+LIMIT 10;
+
+SELECT *
+FROM pg_indexes
+WHERE tablename = 'books';
+
+SELECT *
+FROM pg_indexes
+WHERE tablename = 'customers';
+
+SELECT *
+FROM pg_indexes
+WHERE tablename = 'orders';
+
+CREATE INDEX
+orders_customer_id_idx ON
+orders(customer_id);
+
+CREATE INDEX
+orders_book_id_idx ON
+orders(book_id);
+
+EXPLAIN ANALYZE SELECT original_language,
+ title, sales_in_millions
+FROM books
+WHERE original_language = 'French';
+
+SELECT pg_size_pretty (pg_total_relation_size('books'));
+
+CREATE INDEX 
+books_original_language_title_sales_in_millions_idx ON
+books(original_language, title, sales_in_millions);
+
+EXPLAIN ANALYZE SELECT original_language,
+ title, sales_in_millions
+FROM books
+WHERE original_language = 'French';
+
+SELECT pg_size_pretty (pg_total_relation_size('books'));
+
+DROP INDEX IF EXISTS books_original_language_title_sales_in_millions_idx;
+
+DROP INDEX IF EXISTS orders_customer_id_idx;
+
+DROP INDEX IF EXISTS orders_book_id_idx;
+
+
+SELECT NOW();
+ 
+\COPY orders FROM 'orders_add.txt' DELIMITER ',' CSV HEADER;
+ 
+SELECT NOW();
+
+CREATE INDEX
+orders_customer_id_idx ON
+orders(customer_id);
+
+CREATE INDEX
+orders_book_id_idx ON
+orders(book_id);
+
+CREATE INDEX customers_first_name_last_name_email_address_idx ON
+customers(first_name, last_name, email_address);
+
+
+
